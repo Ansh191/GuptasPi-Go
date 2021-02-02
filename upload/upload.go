facf5aa7d97b7ff0c24c6623171d479c96ececca
Anshul Gupta <ansg191@yahoo.com>
2021-02-01 18:02:59 -0800

    Implement Head endpoint for tus upload

--- upload/upload.go
+++ upload/upload.go
@@ -22,16 +22,18 @@ type Upload struct {
 	ExpirationDate time.Time
 }
 
-var UploadMap = map[uuid.UUID]*Upload{}
+var uploadMap = map[uuid.UUID]*Upload{}
 var lock = sync.RWMutex{}
 
 func AddUploadRouter(r *mux.Router) {
 	r.HandleFunc("/upload/{volume}", startUpload).Methods("POST")
+	r.HandleFunc("/upload/{id}", headUpload).Methods("HEAD")
 }
 
 func startUpload(w http.ResponseWriter, r *http.Request) {
 	volume := mux.Vars(r)["volume"]
 
+	// Process overwrite query
 	overwriteValue := r.FormValue("overwrite")
 	var overwrite bool
 	if overwriteValue != "" {
@@ -47,6 +49,7 @@ func startUpload(w http.ResponseWriter, r *http.Request) {
 		overwrite = false
 	}
 
+	// Tus version header
 	if r.Header.Get("Tus-Resumable") != "1.0.0" {
 		w.WriteHeader(405)
 		return
@@ -54,6 +57,7 @@ func startUpload(w http.ResponseWriter, r *http.Request) {
 
 	metadata := processMetadata(r.Header.Get("Upload-Metadata"))
 
+	// Get filepath
 	b64FilePath, ok := metadata["filename"]
 	if !ok {
 		w.WriteHeader(400)
@@ -78,6 +82,7 @@ func startUpload(w http.ResponseWriter, r *http.Request) {
 
 	var uploadLength uint64
 
+	// Upload length and deferral
 	if deferLength := r.Header.Get("Upload-Defer-Length"); deferLength != "" {
 		if deferLength != "1" {
 			w.WriteHeader(400)
@@ -100,6 +105,7 @@ func startUpload(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	// File creation
 	var file *os.File
 	if !overwrite {
 		file, err = os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0777)
@@ -140,13 +146,13 @@ func startUpload(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Failed to close file: %v", err)
 	}
 
+	// UUID generation
 	id, err := uuid.NewRandom()
 	if err != nil {
 		log.Printf("UUID creation error: %v", err)
 		w.WriteHeader(500)
 		return
 	}
-	idString := strings.ReplaceAll(id.String(), "-", "")
 
 	upload := Upload{
 		FilePath:       filePath,
@@ -156,13 +162,46 @@ func startUpload(w http.ResponseWriter, r *http.Request) {
 	}
 
 	lock.Lock()
-	UploadMap[id] = &upload
+	uploadMap[id] = &upload
 	lock.Unlock()
 
 	w.Header().Add("Tus-Removable", "1.0.0")
-	w.Header().Add("Upload-Exires", upload.ExpirationDate.Format(time.RFC3339))
-	w.Header().Add("Location", "http://localhost:5000/upload/"+idString)
+	w.Header().Add("Upload-Expires", upload.ExpirationDate.Format(time.RFC3339))
+	w.Header().Add("Location", "http://localhost:5000/upload/"+id.String())
 	w.WriteHeader(201)
+}
+
+func headUpload(w http.ResponseWriter, r *http.Request) {
+	idString := mux.Vars(r)["id"]
+
+	id, err := uuid.Parse(idString)
+	if err != nil {
+		log.Printf("Error parsing uuid: %v", err)
+		w.WriteHeader(400)
+		return
+	}
+
+	lock.RLock()
+	upload, ok := uploadMap[id]
+	lock.RUnlock()
+
+	if !ok {
+		w.WriteHeader(404)
+		return
+	}
+
+	w.Header().Add("Upload-Offset", strconv.FormatUint(upload.Offset, 10))
+	w.Header().Add("Upload-Expires", upload.ExpirationDate.Format(time.RFC3339))
+	w.Header().Add("Tus-Resumable", "1.0.0")
+	w.Header().Add("Cache-Control", "no-store")
+
+	if upload.FileSize > 0 {
+		w.Header().Add("Upload-Length", strconv.FormatUint(upload.FileSize, 10))
+	} else {
+		w.Header().Add("Upload-Defer-Length", "1")
+	}
+
+	w.WriteHeader(200)
 }
 
 func processMetadata(metadataText string) map[string]string {
